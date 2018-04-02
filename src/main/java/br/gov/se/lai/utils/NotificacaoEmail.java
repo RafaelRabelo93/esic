7110d5d75d29c6bdf1d70b7e80aff36f46bc181b
Michael Mendonça CGE <michael.mendonca@cge.se.gov.br>
2018-04-02 11:43:22 -0300

    Adição de template de email HTML;

--- src/main/java/br/gov/se/lai/utils/NotificacaoEmail.java
+++ src/main/java/br/gov/se/lai/utils/NotificacaoEmail.java
@@ -2,13 +2,18 @@ package br.gov.se.lai.utils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.io.File;  
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailAttachment;
 import org.apache.commons.mail.EmailConstants;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
+import org.apache.commons.mail.resolver.DataSourceUrlResolver;
+import org.apache.commons.mail.HtmlEmail;
+import org.apache.commons.mail.ImageHtmlEmail;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -30,6 +35,9 @@ import br.gov.se.lai.entity.Usuario;
 public class NotificacaoEmail implements Job{
 	
 
+	private static File img1;
+
+
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 	}
@@ -43,12 +51,66 @@ public class NotificacaoEmail implements Job{
 
 //		List<Mensagem> mensagens = new ArrayList<Mensagem>(MensagemDAO.list(solicitacao.getIdSolicitacao()));	
 //		String mensagem = mensagens.get(mensagens.size()-1).getTexto();
-		String mensagem = "Há uma nova mensagem de "+remetente + " para "+solicitacao.getEntidades().getSigla()+" na solicitação de protocolo "+ 
-						solicitacao.getProtocolo() + " acesse o portal do esic para visualizar. \n Acesse: http://localhost:8080/esic" + "\n"+envio[2]; 
+		StringBuffer msg = new StringBuffer();
+		  msg.append("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">");
+		  msg.append("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
+		  msg.append("<head>");
+		  msg.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />");
+		  msg.append("<title>Redefinição de senha e-SIC</title>");
+		  msg.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>");
+		  msg.append("</head>");
+		  
+		  msg.append("<body style=\"margin: 20px; padding: 0; background-color: #ebecf0 !important; font-family: lato, Sans-serif; font-weight: normal;\">");
+		 
+		  msg.append("<table align=\"center\" border=\"1\" cellpadding=\"0\" cellspacing=\"0\" width=\"600\" style=\"border: 1px solid #d9dbde; border-collapse: collapse;  margin-top: 10px;\">");
+		  
+			  msg.append("<tr>");
+				  msg.append("<td align=\"center\" bgcolor=\"#145187\" style=\"padding: 20px;\">");
+				  	msg.append("<h2 style=\"margin-bottom: 0; color: white;\">e-SIC - SE</h2>");
+				  msg.append("</td>");
+			  msg.append("</tr>");
+			  
+			  msg.append("<tr>");
+				  msg.append("<td bgcolor=\"#ffffff\" style=\"padding: 40px; text-align: center;\">");
+//				  =======================
+//				  == CORPO DA MENSAGEM ==
+//				  =======================
+					  msg.append("<p style=\"font-size: 30px; margin-top: 0\">Nova Mensagem</p>");
+					  msg.append("<p>Há uma nova mensagem de " + remetente + " para " + solicitacao.getEntidades().getSigla() + " na solicitação de protocolo " + solicitacao.getProtocolo() + "</p>");
+					  msg.append("<p>Acesse o portal do esic para visualizar: </p>");
+					  msg.append("<a href=\"http://esic.se.gov.br/\"> http://esic.se.gov.br </a>" );
+					  msg.append("<p>" + envio[2] + "</p>");
+//				  =======================
+//				  =======================
+				  msg.append("</td>");
+			  msg.append("</tr>");
+			  
+			  msg.append("<tr>");
+				  msg.append("<td bgcolor=\"#145187\" style=\"padding: 20px;\">");
+					  msg.append("<table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\">");
+						  msg.append("<tr>");
+							  msg.append("<td>");
+//							  	msg.append("<img src=\"https://i.imgur.com/V73uJdA.png\" height=\"80px\" style=\"display: block;\" />");
+							  msg.append("</td>");
+							  msg.append("<td>");
+								  msg.append("<h2 style=\"margin-bottom: 0; color: white;\">Controladoria-Geral do Estado</h2>");
+								  msg.append("<p style=\"margin-bottom: 0; color: white;\">Governo do Estado de Sergipe</p>");
+							  msg.append("</td>");
+						  msg.append("</tr>");
+					  msg.append("</table>");
+				  msg.append("</td>");
+			  msg.append("</tr>");
+		  
+		  msg.append("</table>");
+		  
+		  msg.append("</body>");
+		  msg.append("</html>");
+		  
+//		String mensagem = "Há uma nova mensagem de "+remetente + " para "+solicitacao.getEntidades().getSigla()+" na solicitação de protocolo "+ 
+//						solicitacao.getProtocolo() + " acesse o portal do esic para visualizar. \n Acesse: http://esic.se.gov.br" + "\n"+envio[2]; 
 
-		
 		try {  
-			enviarEmail(destinatario, titulo, mensagem);
+			enviarEmailHTML(destinatario, titulo, msg.toString());
 			
 		} catch (EmailException e) {  
 			System.out.println(e.getCause());
@@ -125,10 +187,61 @@ public class NotificacaoEmail implements Job{
 //			email.setSmtpPort(465);
 //			email.addTo(resp.getEmail().toString()); //pode ser qualquer email  
 			email.addTo("mayara.machado@cge.se.gov.br"); //pode ser qualquer email  
-			email.setFrom("no_reply@cge.se.gov.br"); //será passado o email que você fará a autenticação 
-			email.setSubject(titulo);  
-			email.setMsg(mensagem);  
-			email.send();  
+			email.setFrom("no_reply@cge.se.gov.br"); //será passado o email que você fará a autenticação
+			
+			StringBuffer msg = new StringBuffer();
+			  msg.append("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">");
+			  msg.append("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
+			  msg.append("<head>");
+			  msg.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />");
+			  msg.append("<title>Redefinição de senha e-SIC</title>");
+			  msg.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>");
+			  msg.append("</head>");
+			  
+			  msg.append("<body style=\"margin: 20px; padding: 0; background-color: #ebecf0 !important; font-family: lato, Sans-serif; font-weight: normal;\">");
+			 
+			  msg.append("<table align=\"center\" border=\"1\" cellpadding=\"0\" cellspacing=\"0\" width=\"600\" style=\"border: 1px solid #d9dbde; border-collapse: collapse;  margin-top: 10px;\">");
+			  
+				  msg.append("<tr>");
+					  msg.append("<td align=\"center\" bgcolor=\"#145187\" style=\"padding: 20px;\">");
+					  	msg.append("<h2 style=\"margin-bottom: 0; color: white;\">e-SIC - SE</h2>");
+					  msg.append("</td>");
+				  msg.append("</tr>");
+				  
+				  msg.append("<tr>");
+					  msg.append("<td bgcolor=\"#ffffff\" style=\"padding: 40px; text-align: center;\">");
+//					  =======================
+//					  == CORPO DA MENSAGEM ==
+//					  =======================
+						  msg.append("<p style=\"font-size: 30px; margin-top: 0\">" + titulo + "</p>");
+						  msg.append("<p>" + mensagem + "</p>");
+//					  =======================
+//					  =======================
+					  msg.append("</td>");
+				  msg.append("</tr>");
+				  
+				  msg.append("<tr>");
+					  msg.append("<td bgcolor=\"#145187\" style=\"padding: 20px;\">");
+						  msg.append("<table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\">");
+							  msg.append("<tr>");
+								  msg.append("<td>");
+//								  	msg.append("<img src=\"https://i.imgur.com/V73uJdA.png\" height=\"80px\" style=\"display: block;\" />");
+								  msg.append("</td>");
+								  msg.append("<td>");
+									  msg.append("<h2 style=\"margin-bottom: 0; color: white;\">Controladoria-Geral do Estado</h2>");
+									  msg.append("<p style=\"margin-bottom: 0; color: white;\">Governo do Estado de Sergipe</p>");
+								  msg.append("</td>");
+							  msg.append("</tr>");
+						  msg.append("</table>");
+					  msg.append("</td>");
+				  msg.append("</tr>");
+			  
+			  msg.append("</table>");
+			  
+			  msg.append("</body>");
+			  msg.append("</html>");
+			
+			  
 		} catch (EmailException e) {  
 			System.out.println(e.getMessage());  
 			System.out.println(email.getSocketConnectionTimeout());  
@@ -187,16 +300,82 @@ public class NotificacaoEmail implements Job{
 	
 
 	
+//	public static void enviarEmailRedefinicaoSenha(String hashcodeUser, String email) {
+//		String mensagem = "Clique no link para redefinir sua senha:  http://localhost:8080/esic/Alterar/redefinir_senha.xhtml?access_key="+hashcodeUser;
+//		try {
+//			enviarEmail(email, "Redefinição de senha", mensagem);
+//
+//		} catch (EmailException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+	
 	public static void enviarEmailRedefinicaoSenha(String hashcodeUser, String email) {
-		String mensagem = "Clique no link para redefinir sua senha:  http://localhost:8080/esic/Alterar/redefinir_senha.xhtml?access_key="+hashcodeUser;
+		String link = "http://esic.se.gov.br/Alterar/redefinir_senha.xhtml?access_key="+hashcodeUser;
 		try {
-			enviarEmail(email, "Redefinição de senha", mensagem);
+			  StringBuffer msg = new StringBuffer();
+			  msg.append("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">");
+			  msg.append("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
+			  msg.append("<head>");
+			  msg.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />");
+			  msg.append("<title>Redefinição de senha e-SIC</title>");
+			  msg.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>");
+			  msg.append("</head>");
+			  
+			  msg.append("<body style=\"margin: 20px; padding: 0; background-color: #ebecf0 !important; font-family: lato, Sans-serif; font-weight: normal;\">");
+			 
+			  msg.append("<table align=\"center\" border=\"1\" cellpadding=\"0\" cellspacing=\"0\" width=\"600\" style=\"border: 1px solid #d9dbde; border-collapse: collapse;  margin-top: 10px;\">");
+			  
+				  msg.append("<tr>");
+					  msg.append("<td align=\"center\" bgcolor=\"#145187\" style=\"padding: 20px;\">");
+					  	msg.append("<h2 style=\"margin-bottom: 0; color: white;\">e-SIC - SE</h2>");
+					  msg.append("</td>");
+				  msg.append("</tr>");
+				  
+				  msg.append("<tr>");
+					  msg.append("<td bgcolor=\"#ffffff\" style=\"padding: 40px; text-align: center;\">");
+						  msg.append("<p style=\"font-size: 30px; margin-top: 0\">Redefinição de Senha</p>");
+						  msg.append("<p>Se você esqueceu sua senha ou deseja redefini-la, use o botão abaixo para fazê-lo.</p>");
+						  msg.append("<a href=\"" + link + "\">" );
+						  msg.append("<button style=\"margin: 20px; color: white; border-radius: 3px; border: 1px solid #1d4a74; padding: 10px; font-size: 20px; background-color: #296099; background: linear-gradient(to bottom right, #135186, #4087dc);\"> Nova Senha</button>");
+						  msg.append("</a>");
+						  msg.append("<p style=\"color: #9E9E9E; margin-bottom: 0\">Se você não requisitou a mudança de senha, ignore este e-mail. Apenas uma pessoa com acesso ao seu e-mail pode redefinir sua senha.</p>");
+						  msg.append("<p style=\"color: #9E9E9E; margin-bottom: 0\">Caso o botão não funcione, clique ou copie o link a seguir no navegador:</p>");
+						  msg.append("<a style=\"color: #9E9E9E;\" href=\"" + link + "\">"+ link +"</a>" );
+					  msg.append("</td>");
+				  msg.append("</tr>");
+				  
+				  msg.append("<tr>");
+					  msg.append("<td bgcolor=\"#145187\" style=\"padding: 20px;\">");
+						  msg.append("<table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\">");
+							  msg.append("<tr>");
+								  msg.append("<td>");
+//								  	msg.append("<img src=\"https://i.imgur.com/V73uJdA.png\" height=\"80px\" style=\"display: block;\" />");
+								  msg.append("</td>");
+								  msg.append("<td>");
+									  msg.append("<h2 style=\"margin-bottom: 0; color: white;\">Controladoria-Geral do Estado</h2>");
+									  msg.append("<p style=\"margin-bottom: 0; color: white;\">Governo do Estado de Sergipe</p>");
+								  msg.append("</td>");
+							  msg.append("</tr>");
+						  msg.append("</table>");
+					  msg.append("</td>");
+				  msg.append("</tr>");
+			  
+			  msg.append("</table>");
+			  
+			  msg.append("</body>");
+			  msg.append("</html>");
+			  
+			  // set the html message
+			  enviarEmailHTML(email, "Redefinição de senha e-SIC SE", msg.toString());
 
 		} catch (EmailException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
 
 	public static void enviarEmailRequisicaoResponsavel(int idUser, int idEntidade, String emailUser,String hashcode, String email, String mensagem) {
 		mensagem += "\n\nClique no link para cadastrar usuário:  http://localhost:8080/esic/Cadastro/cad_responsavel.xhtml?access-key="+hashcode.substring(0, hashcode.length()/2)
@@ -213,6 +392,27 @@ public class NotificacaoEmail implements Job{
 		}
 	}
 	
+	//+++++++++++++++++++ Email HTML
+	public static void enviarEmailHTML(String destinatario, String titulo, String mensagem) throws EmailException {
+		
+		// Create the email message
+		ImageHtmlEmail emailHtml = new ImageHtmlEmail();
+		emailHtml.setDebug(true);
+		emailHtml.setHostName("smtp.expresso.se.gov.br");  
+		emailHtml.setAuthentication("michael.mendonca","015975325");  
+		emailHtml.addTo(destinatario);
+		emailHtml.setFrom("no_reply@cge.se.gov.br"); //será passado o email que você fará a autenticação
+		emailHtml.setSubject(titulo);
+		
+		// set the html message
+		emailHtml.setHtmlMsg(mensagem);
+		
+		// set the alternative message
+		emailHtml.setTextMsg("Seu provedor de email não suporta este tipo de mensagens.");
+		
+		// send the email
+		emailHtml.send();
+	}
 	
 	//+++++++++++++++++++ Email simples
 	public static void enviarEmail(String destinatario, String titulo, String mensagem) throws EmailException {
@@ -233,6 +433,7 @@ public class NotificacaoEmail implements Job{
 		email.send();  
 		
 	}
+	
 	
 
 //	public static void enviarEmail(String destinatario, String remetente, String titulo, String mensagem) throws EmailException {
